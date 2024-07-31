cb1f229975a52f9203bb817885d4749a2a621e81
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2024-08-01 01:10:03 +0530

    api test

--- Backend/api/index.ts
+++ Backend/api/index.ts
@@ -10,22 +10,22 @@
 const express = require("express");
 const app = express();
 //const dotEnv = require("dotenv");
-const cors = require("cors");
+// const cors = require("cors");
 // const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const User = require("../router/userRouter");
-const Post = require("../router/postRouter");
-// const path = require("path");
-const Comments = require("../router/commentsRouter");
+// const Post = require("../router/postRouter");
+// // const path = require("path");
+// const Comments = require("../router/commentsRouter");
 
-app.use(cors());
+// app.use(cors());
 
 // app.use(bodyParser.json());
 
 //dotEnv.config({path:"./env/.env"});             process.env.MONGO_DB_CLOUD_URL
 
 //const port = precess.env.PORT || 5000;
-const port = 8000;
+// const port = 8000;
 
 // mongoose
 //   .connect(
@@ -48,9 +48,9 @@ const port = 8000;
 // }
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use("/api/users", User);
-//app.use("/api/profiles",require("./router/profileRouter"));
-app.use("/api/posts", Post);
-app.use("/api/comments", Comments);
+// //app.use("/api/profiles",require("./router/profileRouter"));
+// app.use("/api/posts", Post);
+// app.use("/api/comments", Comments);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
