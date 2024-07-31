07601d93b36e6586941493fc654feb43310288ef
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2024-08-01 04:04:31 +0530

    add task issue fixed

--- Backend/models/Post.js
+++ Backend/models/Post.js
@@ -4,7 +4,7 @@ const PostSchema = new mongoose.Schema(
     user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
     title: { type: String, required: true },
     description: { type: String, required: true },
-    username: { type: String, required: true },
+    // username: { type: String, required: true },
 
     // comments:[
     //
