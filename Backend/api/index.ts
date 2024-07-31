d37b74eb3d439ee008b49195b6b1583230705cb8
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2024-08-01 00:33:09 +0530

    api fix

--- Backend/api/index.ts
+++ Backend/api/index.ts
@@ -11,16 +11,16 @@ const express = require("express");
 const app = express();
 //const dotEnv = require("dotenv");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const User = require("../router/userRouter");
 const Post = require("../router/postRouter");
-const path = require("path");
+// const path = require("path");
 const Comments = require("../router/commentsRouter");
 
 app.use(cors());
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 //dotEnv.config({path:"./env/.env"});             process.env.MONGO_DB_CLOUD_URL
 
