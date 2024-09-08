5afec23f769a5ce808355fdbe6c7653bf3c79554
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2024-09-09 02:00:59 +0530

    app complete

--- Backend/api/index.ts
+++ Backend/api/index.ts
@@ -1,46 +1,30 @@
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req, res) => res.send("Express on Vercel"));
-
-// app.listen(3000, () => console.log("Server ready on port 3000."));
-
-// module.exports = app;
-
-const express = require("express");
-const app = express();
-//const dotEnv = require("dotenv");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const User = require("../router/userRouter");
-const Post = require("../router/postRouter");
-// const path = require("path");
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const User = require("../router/userRouter")
+const Post = require("../router/postRouter")
 
 const allowedOrigins = [
   "https://jira-clone-btbv.vercel.app",
   "https://jira-clone-kohl.vercel.app",
   "http://localhost:3000",
-];
+]
 
 const corsOptions = {
   origin: function (origin, callback) {
-    console.log("Origin:", origin);
+    console.log("Origin:", origin)
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error("Not allowed by CORS"))
     }
   },
-};
-app.use(cors(corsOptions));
+}
+app.use(cors(corsOptions))
 
-app.use(bodyParser.json());
-
-//dotEnv.config({path:"./env/.env"});             process.env.MONGO_DB_CLOUD_URL
-
-//const port = precess.env.PORT || 5000;
-// const port = 8000;
+app.use(bodyParser.json())
 
 mongoose
   .connect(
@@ -51,21 +35,16 @@ mongoose
     }
   )
   .then((response) => {
-    console.log("Connected to Mongo");
+    console.log("Connected to Mongo")
   })
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
 
-// if(process.env.NODE_ENV === "production"){
+app.use("/api/users", User)
+app.use("/api/posts", Post)
 
-// }
-app.get("/", (req, res) => res.send("Express on Vercel"));
-app.use("/api/users", User);
-// //app.use("/api/profiles",require("./router/profileRouter"));
-app.use("/api/posts", Post);
+app.listen(8000)
 
-app.listen(8000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
+module.exports = app
